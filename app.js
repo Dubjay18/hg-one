fc6b249f6a90e1869fa8a9abf2a0a50b05ee62b0
Dubjay18 <jejeniyi7@gmail.com>
2023-09-09 18:18:39 +0100

    done

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const moment = require("moment");
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
+
+  // Get the current day of the week
+  const currentDay = moment().utc().format("dddd");
+
+  // Get the current UTC time within a +/- 2 minute window
+  const currentUtcTime = moment()
+    .utc()
+    .format("YYYY-MM-DDTHH:mm:ss[Z]");
+
+  // Define GitHub file and repo URLs
+  const githubFileUrl =
+    "https://github.com/Dubjay18/hg-one/index.js";
+  const githubRepoUrl =
+    "https://github.com/Dubjay18/hg-one";
+
+  // Create the response JSON
+  const response = {
+    slack_name: slackName,
+    current_day: currentDay,
+    utc_time: currentUtcTime,
+    track: track,
+    github_file_url: githubFileUrl,
+    github_repo_url: githubRepoUrl,
+    status_code: 200,
+  };
+
+  res.json(response);
+});
+module.exports = app;
--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 8000;
-const utcOffset = 2 * 60 * 1000; // +/-2 minutes in milliseconds
+const http = require("http");
+const { PORT } = require("./config/constants");
+const app = require("./app");
+const secureServerListen = require("./config/server");
 
-app.use(express.json());
+/**
+ * The http server instance.
+ */
+const server = http.createServer(app);
 
-app.get("/api", (req, res) => {
-  const { slack_name = "", track = "" } = req.query;
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const now = new Date(Date.now() + utcOffset);
-  const response = {
-    slack_name,
-    current_day: daysOfWeek[now.getUTCDay()],
-    utc_time: now.toISOString(),
-    track,
-    github_file_url:
-      "https://github.com/Dubjay18/hg-one/index.js",
-    github_repo_url: "https://github.com/Dubjay18/hg-one",
-    status_code: 200,
-  };
-  res.json(response);
-});
-
-app.listen(port, () =>
-  console.log(`Server is running on port ${port}`)
-);
+/**
+ * Starts the secure server listening on the specified port.
+ */
+secureServerListen(PORT, server);
